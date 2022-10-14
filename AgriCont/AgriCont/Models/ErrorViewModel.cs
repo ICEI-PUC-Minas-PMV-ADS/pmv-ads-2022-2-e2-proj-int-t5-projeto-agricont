09e1e49c6cfb8d3aed09e80182f8a3ecb32c420f
ClaudiaCarapiaFerraz <102997121+ClaudiaCarapiaFerraz@users.noreply.github.com>
2022-10-14 13:42:24 -0300

    Criação projeto inicial

---
+++ AgriCont/AgriCont/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AgriCont.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
