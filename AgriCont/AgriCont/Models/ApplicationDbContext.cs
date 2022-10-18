08842ac6ff0139937e3f7e6303347ea9cf19b78a
Karen Noguti <jkjvida@hotmail.com>
2022-10-18 15:32:43 +0900

    Configuração do DBConstext e ConnectingString

--- AgriCont/AgriCont/Models/ApplicationDbContext.cs
+++ AgriCont/AgriCont/Models/ApplicationDbContext.cs
@@ -8,9 +8,11 @@ namespace AgriCont.Models
 {
     public class ApplicationDbContext : DbContext
     {
+#pragma warning disable CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+#pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
         {
-        }
 
         }
+    }
 }
