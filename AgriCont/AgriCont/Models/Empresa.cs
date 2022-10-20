d54e9a579819c40cfcaf133e58cdc68b554f63b8
Karen Noguti <jkjvida@hotmail.com>
2022-10-21 08:56:57 +0900

    Alteração no Model -Empresa

--- AgriCont/AgriCont/Models/Empresa.cs
+++ AgriCont/AgriCont/Models/Empresa.cs
@@ -9,36 +9,37 @@ namespace AgriCont.Models
         [Key]
         public int Id { get; set; }
 
-        [Display(Name = "Razão Social")]
+        [Display(Name = "Razão Social (*)")]
         [Required(ErrorMessage = "Obrigatório informar a Razão Social!")]
         public string? RazaoSocial { get; set; }
 
-        [Display(Name = "Nome Fantasia")]
+        [Display(Name = "Nome Fantasia (*)")]
         [Required(ErrorMessage = "Obrigatório informar o Nome Fantasia!")]
         public string? NomeFantasia { get; set; }
 
-        [Display(Name = "CNPJ")]
+        [Display(Name = "CNPJ (*)")]
         [Required(ErrorMessage = "Obrigatório informar o CNPJ!")]
         public string? Cnpj { get; set; }
 
-
+        [Display(Name = "Logradouro (*)")]
         [Required(ErrorMessage = "Obrigatório informar o Logradouro!")]
         public string? Logradouro { get; set; }
 
-        [Display(Name = "Número")]
+        [Display(Name = "Número (*)")]
         [Required(ErrorMessage = "Obrigatório informar o Número!")]
         public string? Numero { get; set; }
 
         public string? Complemento { get; set; }
 
-        [Display(Name = "CEP")]
+        [Display(Name = "CEP(*)")]
         [Required(ErrorMessage = "Obrigatório informar o CEP!")]
         public string? Cep { get; set; }
 
+        [Display(Name = "Telefone (*)")]
         [Required(ErrorMessage = "Obrigatório informar o Telefone!")]
         public string? Telefone { get; set; }
 
-        [Display(Name = "E-mail")]
+        [Display(Name = "E-mail (*)")]
         [Required(ErrorMessage = "Obrigatório informar o Email!")]
         public string? Email { get; set; }
     }
