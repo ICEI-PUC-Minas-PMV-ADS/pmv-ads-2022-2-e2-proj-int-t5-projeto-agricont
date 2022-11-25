44cf1810a8a03bbe9e61970237a9f29c01545be5
keirosu <103314640+keirosu@users.noreply.github.com>
2022-11-25 09:13:19 +0900

    Alterar Margin do botão delete

--- AgriCont/AgriCont/Models/ApplicationDbContext.cs
+++ AgriCont/AgriCont/Models/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace AgriCont.Models
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 #pragma warning restore CS8618 // O campo não anulável precisa conter um valor não nulo ao sair do construtor. Considere declará-lo como anulável.
         {
-
+            Database.EnsureCreated();
         }
 
         public DbSet<Empresa> Empresas { get; set; }
